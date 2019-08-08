5ccee6b81111a9ea1b420c63cd83e7317020cd9f
Marc Schlossberg <mr.schlossberg@gmail.com>
2019-08-07 19:42:58 -0700

    Remove logger

--- frontend/store/store.jsx
+++ frontend/store/store.jsx
@@ -9,8 +9,8 @@ const configureStore = (preloadedState = {} ) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk,logger)
-        // applyMiddleware(thunk)
+        // applyMiddleware(thunk,logger)
+        applyMiddleware(thunk)
     )
 
 );
